43393c893f6da35282d6524862cb0a0f5ab68e8b
frc-3461 <programming@peacce.org>
2024-03-22 18:08:03 -0400

    update wpilib

--- src/main/java/frc/lib/telemetry/BuildConstants.java
+++ src/main/java/frc/lib/telemetry/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "frc-2024";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 303;
-    public static final String GIT_SHA = "5a374bb71a198fc01dfe92326efba384f4953ddf";
-    public static final String GIT_DATE = "2024-03-21 21:33:26 EDT";
-    public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2024-03-21 22:24:55 EDT";
-    public static final long BUILD_UNIX_TIME = 1711074295057L;
+    public static final int GIT_REVISION = 306;
+    public static final String GIT_SHA = "e19d562df69bbf454727fb8889c75411db8e1939";
+    public static final String GIT_DATE = "2024-03-22 17:54:16 EDT";
+    public static final String GIT_BRANCH = "WNE";
+    public static final String BUILD_DATE = "2024-03-22 18:07:22 EDT";
+    public static final long BUILD_UNIX_TIME = 1711145242330L;
     public static final int DIRTY = 1;
 
     private BuildConstants(){}
