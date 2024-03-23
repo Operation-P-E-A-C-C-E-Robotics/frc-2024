bdb9948d42dd664c2ae5923d9d525e60591bb66f
frc-3461 <programming@peacce.org>
2024-03-23 10:06:31 -0400

    increase swerve current limit

--- src/main/java/frc/lib/telemetry/BuildConstants.java
+++ src/main/java/frc/lib/telemetry/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "frc-2024";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 308;
-    public static final String GIT_SHA = "9d493ad657eeaadf80b40d9c8e75346f18e002a4";
-    public static final String GIT_DATE = "2024-03-22 18:18:51 EDT";
+    public static final int GIT_REVISION = 309;
+    public static final String GIT_SHA = "d93fe16ea93af62a1005a4e3796ba019f679ef66";
+    public static final String GIT_DATE = "2024-03-23 10:05:43 EDT";
     public static final String GIT_BRANCH = "WNE";
-    public static final String BUILD_DATE = "2024-03-23 09:21:00 EDT";
-    public static final long BUILD_UNIX_TIME = 1711200060255L;
+    public static final String BUILD_DATE = "2024-03-23 10:06:11 EDT";
+    public static final long BUILD_UNIX_TIME = 1711202771419L;
     public static final int DIRTY = 1;
 
     private BuildConstants(){}
